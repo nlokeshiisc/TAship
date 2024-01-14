b3ffbb9b5f94b0494069a5853b61712ad7f38631
Lokesh N <nlokeshiisc@gmail.com>
2024-01-14 17:28:32 +0530

    Pushing working code

--- dataset.py
+++ dataset.py
@@ -2,8 +2,9 @@ from abc import ABC, abstractmethod, abstractproperty
 import torch
 from torch.utils.data import Dataset, DataLoader
 
+
 class AbsBinaryData(ABC, Dataset):
-    def __init__(self, dim, num) -> None:
+    def __init__(self, dim, num, msb_first=False) -> None:
         """Represents a data object that holds binary data.
 
         Args:
@@ -13,29 +14,30 @@ class AbsBinaryData(ABC, Dataset):
         super().__init__()
         self.dim = dim
         self.num = num
-        
+
         self.x1 = self.gen_x()
         self.x2 = self.gen_x()
 
         self.y = None
-        
+        self.msb_first = msb_first
+
     def __len__(self):
         return self.num
-    
+
     def gen_x(self):
         """Generates the x."""
         # generate binary array of shape (num, dim)
         return torch.randint(2, size=(self.num, self.dim))
-    
+
     def x_to_str(self, x):
         """Converts the x to a string."""
-        return ''.join([str(i) for i in x])
-    
+        return "".join([str(i) for i in x])
+
     @abstractmethod
     def gen_y(self):
         """Generates the y for the x."""
         raise NotImplementedError
-    
+
     def binary_to_decimal(self, binary_tensor):
         """Converts a binary tensor to a decimal tensor.
 
@@ -49,7 +51,7 @@ class AbsBinaryData(ABC, Dataset):
         for i in range(binary_tensor.size(1)):
             decimal_tensor = decimal_tensor * 2 + binary_tensor[:, i]
         return decimal_tensor
-    
+
     def decimal_to_binary(self, decimal_tensor):
         """converts a decimal tensor to a binary tensor.
 
@@ -62,29 +64,44 @@ class AbsBinaryData(ABC, Dataset):
         binary_tensor = torch.zeros(decimal_tensor.size(0), self.dim, dtype=torch.int)
         for i in range(self.dim - 1, -1, -1):
             binary_tensor[:, i] = decimal_tensor % 2
-            decimal_tensor = decimal_tensor // 2        
+            decimal_tensor = decimal_tensor // 2
         assert binary_tensor.size() == (len(decimal_tensor), self.dim), "size mismatch"
         return binary_tensor
-
-    
-    def __getitem__(self, index):
-        raise NotImplementedError
-
-class BinaryAddData(AbsBinaryData):
-    def __init__(self, dim, num, msb_first=True) -> None:
-        super().__init__(dim, num)
-        self.y = self.gen_y()
-        self.msb_first = msb_first # determines the first bit that is input to the rnn
-    
-    def gen_y(self):
-        y = self.binary_to_decimal(self.x1) + self.binary_to_decimal(self.x2)
-        return self.decimal_to_binary(y)
 
     def __getitem__(self, index):
         x1 = self.x1[index]
         x2 = self.x2[index]
-        x = torch.stack([x1.view(1, -1), x2.view(1, -1)], dim=0).T.squeeze()
+        x = torch.cat([x1.view(1, -1), x2.view(1, -1)], dim=0)
+        y = self.y[index].view(1, -1)
         if self.msb_first == False:
             x = torch.flip(x, dims=[1])
-        y = self.y[index]
-        return x, y
+            y = torch.flip(y, dims=[1])
+        # make the time axis as rows
+        return x.T, y.T
+
+
+class BinaryAddData(AbsBinaryData):
+    """This is for the binary addition problem. The x is a 2 x dim tensor, and the y is a 1 x dim tensor."""
+    def __init__(self, dim, num, msb_first=False) -> None:
+        super().__init__(dim, num, msb_first)
+        self.y = self.gen_y()
+
+    def gen_y(self):
+        y = self.binary_to_decimal(self.x1) + self.binary_to_decimal(self.x2)
+        return self.decimal_to_binary(y)
+
+
+
+class BinaryGTData(AbsBinaryData):
+    """This is for the binary greater than problem. The x is a 2 x dim tensor, and the y is a 1 x 1 tensor."""
+    def __init__(self, dim, num, msb_first=True) -> None:
+        super().__init__(dim, num, msb_first)
+        self.y = self.gen_y()
+    
+    def gen_y(self):
+        x1 = self.binary_to_decimal(self.x1)
+        x2 = self.binary_to_decimal(self.x2)
+        y = (x1 > x2).to(dtype=torch.int)
+        return y.view(-1, 1)
+    
+        
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+
 torch.manual_seed(0)
 import torch.nn as nn
 from dataset import BinaryAddData
@@ -6,31 +7,37 @@ import constants
 import logging
 
 logging.basicConfig(
-    filename='binary_ops.log',
-    format='%(asctime)s :: %(filename)s:%(funcName)s :: %(message)s',
-    filemode='w',
+    filename="binary_ops.log",
+    format="%(asctime)s :: %(filename)s:%(funcName)s :: %(message)s",
+    filemode="a",
 )
 logger = logging.getLogger(name="binary_ops")
 logger.setLevel(logging.DEBUG)
 
-device = "cuda:0"
+logger.info("****" * 20)
+print("inference with more bits")
+
+device = "cuda:6"
 
 params = {
     constants.BSZ: 16,
     constants.NUM_BITS: 10,
-    constants.NUM_TRN: 100, # 'num_samples
+    constants.NUM_TRN: 100,  # 'num_samples
     constants.NUM_TST: 100,
     constants.HIDSIZE: 4,
     constants.LR: 0.001,
-    constants.NUM_EPOCHS: 10000
+    constants.NUM_EPOCHS: 2000,
 }
+
 
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
 
         # Define an RNN layer
-        self.rnn = nn.RNN(input_size=input_size, hidden_size=hidden_size, batch_first=True)
+        self.rnn = nn.RNN(
+            input_size=input_size, hidden_size=hidden_size, batch_first=True
+        )
 
         # Define a fully connected layer for output
         self.fc = nn.Linear(hidden_size, output_size)
@@ -47,14 +54,25 @@ class RNN(nn.Module):
 
         return output
 
-full_adder = RNN(input_size=2, hidden_size=params[constants.HIDSIZE], output_size=1).to(device)
-adder_trn = BinaryAddData(dim=params[constants.NUM_BITS], num=params[constants.NUM_TRN], msb_first=False)
-adder_tst = BinaryAddData(dim=params[constants.NUM_BITS], num=params[constants.NUM_TST], msb_first=False)
-trn_loader = torch.utils.data.DataLoader(adder_trn, batch_size=params[constants.BSZ], shuffle=True)
-tst_loader = torch.utils.data.DataLoader(adder_tst, batch_size=params[constants.NUM_TST], shuffle=False)
+
+full_adder = RNN(input_size=2, hidden_size=params[constants.HIDSIZE], output_size=1).to(
+    device
+)
+adder_trn = BinaryAddData(
+    dim=params[constants.NUM_BITS], num=params[constants.NUM_TRN], msb_first=False
+)
+adder_tst = BinaryAddData(
+    dim=params[constants.NUM_BITS], num=params[constants.NUM_TST], msb_first=False
+)
+trn_loader = torch.utils.data.DataLoader(
+    adder_trn, batch_size=params[constants.BSZ], shuffle=True
+)
+tst_loader = torch.utils.data.DataLoader(
+    adder_tst, batch_size=params[constants.NUM_TST], shuffle=False
+)
 optim = torch.optim.Adam(full_adder.parameters(), lr=params[constants.LR])
 
-logger.info(f'params: {params}')
+logger.info(f"params: {params}")
 
 for epoch in range(params[constants.NUM_EPOCHS]):
     for x, y in trn_loader:
@@ -67,7 +85,17 @@ for epoch in range(params[constants.NUM_EPOCHS]):
         optim.step()
     if (epoch + 1) % 100 == 0:
         print(f'Epoch [{epoch+1}/{params["num_epochs"]}], Loss: {loss.item():.4f}')
-        logger.info(f'Epoch [{epoch+1}/{params["num_epochs"]}], Loss: {loss.item():.4f}')
+        logger.info(
+            f'Epoch [{epoch+1}/{params["num_epochs"]}], Loss: {loss.item():.4f}'
+        )
+
+# Print the weights
+logger.info(f"RNN Parameters")
+for name, parameter in full_adder.rnn.named_parameters():
+    logger.info(f"{name}: {parameter[0].cpu()}")
+logger.info(f"H -> out parameters")
+for name, parameter in full_adder.fc.named_parameters():
+    logger.info(f"{name}: {parameter[0].cpu()}")
 
 with torch.no_grad():
     for x, y in tst_loader:
@@ -75,5 +103,5 @@ with torch.no_grad():
         y = y.to(dtype=torch.float32, device=device)
         output_sequence = full_adder(x)
         loss = nn.MSELoss()(output_sequence, y)
-        print(f'Test loss: {loss.item():.4f}')
-        logger.info(f'Test loss: {loss.item():.4f}')
+        print(f"Test loss: {loss.item():.4f}")
+        logger.info(f"Test loss: {loss.item():.4f}")
