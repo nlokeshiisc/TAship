669a80d2064c806b7bb2fee95df71716f51ca270
Lokesh N <nlokeshiisc@gmail.com>
2024-03-18 09:29:02 +0530

    Pushing rnn analysis code

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import torch
 
 torch.manual_seed(0)
 import torch.nn as nn
-from dataset import BinaryAddData
+from dataset import BinaryAddData, BinaryGTData
 import constants
 import logging
 
+task = "add"
+
+log_file = (
+    "binary_gt.log" if task == "gt" else "binary_add.log" if task == "add" else None
+)
 logging.basicConfig(
-    filename="binary_ops.log",
+    filename=log_file,
     format="%(asctime)s :: %(filename)s:%(funcName)s :: %(message)s",
     filemode="a",
 )
@@ -15,7 +20,7 @@ logger = logging.getLogger(name="binary_ops")
 logger.setLevel(logging.DEBUG)
 
 logger.info("****" * 20)
-print("inference with more bits")
+print("")
 
 device = "cuda:6"
 
@@ -26,8 +31,27 @@ params = {
     constants.NUM_TST: 100,
     constants.HIDSIZE: 4,
     constants.LR: 0.001,
-    constants.NUM_EPOCHS: 2000,
+    constants.NUM_EPOCHS: 3000,
+    constants.STEP_ACT: False,
+    constants.TRAIN_MODEL: True,
 }
+
+
+class StepAct(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def forward(self, input):
+        return torch.relu(torch.sign(input))
+
+
+class StepAct_2(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def forward(self, input):
+        scalar = 1.0
+        return torch.sigmoid(scalar * torch.sign(input))
 
 
 class RNN(nn.Module):
@@ -36,11 +60,13 @@ class RNN(nn.Module):
 
         # Define an RNN layer
         self.rnn = nn.RNN(
-            input_size=input_size, hidden_size=hidden_size, batch_first=True
+            input_size=input_size, hidden_size=hidden_size, batch_first=True, nonlinearity="relu"
         )
 
         # Define a fully connected layer for output
         self.fc = nn.Linear(hidden_size, output_size)
+        self.step = StepAct()
+        # self.step = StepAct_2()
 
     def forward(self, input_sequence):
         # Input sequence shape: (batch_size, sequence_length, input_size)
@@ -52,56 +78,76 @@ class RNN(nn.Module):
         # Fully connected layer for output
         output = self.fc(rnn_output)
 
+        if params[constants.STEP_ACT]:
+            output = self.step(output)
+
         return output
 
 
-full_adder = RNN(input_size=2, hidden_size=params[constants.HIDSIZE], output_size=1).to(
+model = RNN(input_size=2, hidden_size=params[constants.HIDSIZE], output_size=1).to(
     device
 )
-adder_trn = BinaryAddData(
-    dim=params[constants.NUM_BITS], num=params[constants.NUM_TRN], msb_first=False
-)
-adder_tst = BinaryAddData(
-    dim=params[constants.NUM_BITS], num=params[constants.NUM_TST], msb_first=False
-)
+if task == "add":
+    data_trn = BinaryAddData(
+        dim=params[constants.NUM_BITS], num=params[constants.NUM_TRN], msb_first=False
+    )
+    data_tst = BinaryAddData(
+        dim=params[constants.NUM_BITS], num=params[constants.NUM_TST], msb_first=False
+    )
+elif task == "gt":
+    data_trn = BinaryGTData(
+        dim=params[constants.NUM_BITS], num=params[constants.NUM_TRN], msb_first=True
+    )
+    data_tst = BinaryGTData(
+        dim=params[constants.NUM_BITS], num=params[constants.NUM_TST], msb_first=True
+    )
+
 trn_loader = torch.utils.data.DataLoader(
-    adder_trn, batch_size=params[constants.BSZ], shuffle=True
+    data_trn, batch_size=params[constants.BSZ], shuffle=True
 )
 tst_loader = torch.utils.data.DataLoader(
-    adder_tst, batch_size=params[constants.NUM_TST], shuffle=False
+    data_tst, batch_size=params[constants.NUM_TST], shuffle=False
 )
-optim = torch.optim.Adam(full_adder.parameters(), lr=params[constants.LR])
+optim = torch.optim.Adam(model.parameters(), lr=params[constants.LR])
 
 logger.info(f"params: {params}")
 
-for epoch in range(params[constants.NUM_EPOCHS]):
-    for x, y in trn_loader:
-        x = x.to(dtype=torch.float32, device=device)
-        y = y.to(dtype=torch.float32, device=device)
-        optim.zero_grad()
-        output_sequence = full_adder(x)
-        loss = nn.MSELoss()(output_sequence.squeeze(), y.squeeze())
-        loss.backward()
-        optim.step()
-    if (epoch + 1) % 100 == 0:
-        print(f'Epoch [{epoch+1}/{params["num_epochs"]}], Loss: {loss.item():.4f}')
-        logger.info(
-            f'Epoch [{epoch+1}/{params["num_epochs"]}], Loss: {loss.item():.4f}'
-        )
+if params[constants.TRAIN_MODEL]:
+    model.train()
+    for epoch in range(params[constants.NUM_EPOCHS]):
+        for x, y in trn_loader:
+            x = x.to(dtype=torch.float32, device=device)
+            y = y.to(dtype=torch.float32, device=device)
+            optim.zero_grad()
+            output_sequence = model(x)
+            # select the last output of the sequence if task is gt
+            if task == "gt":
+                output_sequence = output_sequence[:, -1, :]
+            loss = nn.MSELoss()(output_sequence.squeeze(), y.squeeze())
+            loss.backward()
+            optim.step()
+        if (epoch + 1) % 100 == 0:
+            print(f'Epoch [{epoch+1}/{params["num_epochs"]}], Loss: {loss.item():.4f}')
+            logger.info(
+                f'Epoch [{epoch+1}/{params["num_epochs"]}], Loss: {loss.item():.4f}'
+            )
+    torch.save(model.state_dict(), "rnn_model.pth")
+else:
+    model.load_state_dict(torch.load("rnn_model.pth"))
 
-# Print the weights
-logger.info(f"RNN Parameters")
-for name, parameter in full_adder.rnn.named_parameters():
-    logger.info(f"{name}: {parameter[0].cpu()}")
-logger.info(f"H -> out parameters")
-for name, parameter in full_adder.fc.named_parameters():
-    logger.info(f"{name}: {parameter[0].cpu()}")
+model.eval()
 
 with torch.no_grad():
     for x, y in tst_loader:
         x = x.to(dtype=torch.float32, device=device)
         y = y.to(dtype=torch.float32, device=device)
-        output_sequence = full_adder(x)
-        loss = nn.MSELoss()(output_sequence, y)
+        output_sequence = model(x)
+        if task == "gt":
+            output_sequence = output_sequence[:, -1, :]
+        loss = nn.MSELoss()(output_sequence.squeeze(), y.squeeze())
         print(f"Test loss: {loss.item():.4f}")
         logger.info(f"Test loss: {loss.item():.4f}")
+        
+        ydecimal = [data_tst.binary_to_decimal(y[i]) for i in range(y.size(0))]
+        ypreddecimal = [data_tst.binary_to_decimal(output_sequence[i]) for i in range(output_sequence.size(0))]
+        loss = nn.MSELoss()(torch.tensor(ydecimal), torch.tensor(ypreddecimal)) 
